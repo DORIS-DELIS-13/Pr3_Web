193bc7e4a9a224b4671c0055e1f4b055d08b6892
DORIS-DELIS-13 <dorisdelis.13@gmail.com>
2019-09-27 10:00:46 +0300

    pleasepleaseplease

--- server.js
+++ server.js
@@ -61,8 +61,7 @@ app.post('/api/articles', function(req, res) {
 });
 
 app.get('/api/articles/:id', function(req, res) {
-	return ArticleModel.findById(req.params.id, function (err, article) 
-	{
+	return ArticleModel.findById(req.params.id, function (err, article) {
 		if (!article) {
 			res.statusCode = 404;
 			return res.send({ error: 'Not found' });
@@ -78,8 +77,7 @@ app.get('/api/articles/:id', function(req, res) {
 });
 
 app.put('/api/articles/:id', function (req, res) {
-	return ArticleModel.findById(req.params.id, function (err, article) 
-	{
+	return ArticleModel.findById(req.params.id, function (err, article) {
 		if (!article) {
 			res.statusCode = 404;
 			return res.send({ error: 'Not found' });
